e637169d8e0379b814a1af9c2a4c6e0d81d59495
karthik dasari <karthiktinku@byuh.edu>
2025-04-09 23:24:09 -1000

    final

--- final/processing.js
+++ final/processing.js
@@ -4,7 +4,7 @@ document.getElementById('myForm').addEventListener('submit',function(event){
 
     const fullname= document.getElementById('fullname').value;
     const email= document.getElementById('email').value;
-    const pass = document.getElementById('password').value;
+   
 
     if(!fullname){
         alert("you need to enter your name.");
@@ -16,32 +16,32 @@ document.getElementById('myForm').addEventListener('submit',function(event){
         return;
 
     }
-    if(!pass){
-        alert("you need to enter your pass.");
-        return;
-
-    }
 
     const formData = {
         fullname: fullname,
         email: email,
-        password: pass,
+    
     }
 
     const xhr = new XMLHttpRequest ();
-    xhr.open ("POST", "submit. json", true);
+    xhr.open ("GET", "submit.json", true);
     xhr.setRequestHeader ("Content-Type", "application/json; charset=UTF-8");
     xhr.onreadystatechange = function () {
         if (xhr. readyState === 4 && xhr.status == 200) {
-            alert ('Form submitted successfully.');
+            //alert ('Form submitted successfully.');
+            const response = JSON.parse(xhr.responseText);
+            document.getElementById("message").innerHTML = response.message;
+            document.getElementById("myForm").innerHTML = "";
+
         } else if (xhr.readyState === 4){ 
             alert ('Error submitting form.');
             //event.preventDefault(); this prevents form submission
 
         }
+
     };
     xhr.send(JSON. stringify(formData));
-    alert("Form Submitted");
+    //alert("Form Submitted");
     console.log(formData);
 
     
